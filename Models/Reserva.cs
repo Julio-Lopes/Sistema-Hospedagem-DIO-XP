511bb566865428e00805d8e880a5af25e0351203
Julio-Lopes <juliocrlopes4@gmail.com>
2024-10-08 14:53:49 -0300

    first commit

---
+++ Models/Reserva.cs
@@ -0,0 +1,50 @@
+
+public class Reserva {
+    public List<Pessoa> Hospedes { get; set; }
+    public Suite Suite { get; set; }
+    public int DiasReservados  { get; set; }
+
+    public Reserva(){}
+
+    public Reserva(int diasReservados )
+    {
+        DiasReservados = diasReservados ;
+    }
+
+    public void CadastrarHospedes(List<Pessoa> hospedes)
+    {
+        if(hospedes.Count <= Suite.Capacidade)
+        {
+            Hospedes = hospedes ;
+        }
+        else
+        {
+            Console.WriteLine("A quantidade de hóspedes excede a capacidade da suíte.");
+        }
+    }
+
+    public void CadastrarSuite(Suite suite)
+    {
+        Suite = suite;
+    }
+
+    public int ObterQuantidadeHospedes()
+    {
+        return Hospedes.Count;
+    }
+
+    public decimal CalcularValorDiaria()
+    {
+        decimal valor = DiasReservados * Suite.ValorDiaria;
+
+        if( DiasReservados >= 10)
+        {   
+            
+            return valor * 0.9m;
+        }
+        else
+        {
+            return valor;
+        }
+    }
+}
